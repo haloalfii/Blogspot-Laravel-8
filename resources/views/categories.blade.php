8a90bf4337025c7b883e35b9f10223745a9c0e2d
haloalfii <alfiankurniawan85@gmail.com>
2021-09-15 16:28:08 +0700

    V-12 Done, Categories Field Clear

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,14 +2,24 @@
 
 @section('container')
     <h1>Post Categories</h1>
-    <br/>
-    @foreach ($categories as $category)
-    <ul>
-        <li>
-            <h5><a style="text-decoration: none" class="text-dark" href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h5>
-        </li>
-    </ul>
-        
-    @endforeach
+
+    <div class="container">
+        <div class="row">
+            @foreach ($categories as $category)
+
+            <div class="col-md-4">
+                <a href="/categories/{{ $category->slug }}">
+                <div class="card bg-dark text-white">
+                    <img src="https://source.unsplash.com/500x500?{{ $category->name }}" alt="{{ $category->name }}">
+                    <div class="card-img-overlay d-flex align-items-center p-0">
+                        <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">{{ $category->name }}</h5>
+                    </div>
+                </div>
+                </a>
+            </div>
+                            
+            @endforeach
+        </div>
+    </div>
 
 @endsection
