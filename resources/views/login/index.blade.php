95fdbaaaa5ea87ba9df2a7afd490a852973c32d5
haloalfii <alfiankurniawan85@gmail.com>
2021-09-19 19:42:36 +0700

    Login, Add Controller

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -14,14 +14,20 @@
         <main class="form-signin">
             <h1 class="bi bi-app-indicator text-center"></h1>
             <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
-            <form>
+            <form action="/login" method="POST">
+                @csrf
                 <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
+                <label for="email">Email address</label>
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror   
                 </div>
                 <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+                <label for="password">Password</label>
                 </div>
 
                 <button class="w-100 btn btn-lg btn-danger" type="submit">Login</button>
