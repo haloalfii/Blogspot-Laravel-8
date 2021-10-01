06afd0eea8c2c1acf8c24c9908bad6d502b823d6
haloalfii <alfiankurniawan85@gmail.com>
2021-10-01 12:58:01 +0700

    v-18, nyicil view Create Post

---
+++ resources/views/dashboard/posts/create.blade.php
@@ -0,0 +1,23 @@
+@extends('dashboard.layouts-dash.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Create New Post</h1>
+    </div>
+
+    <div class="col-lg-8">
+        @csrf
+        <form method="POST" action="/dashboard/posts">
+            <div class="mb-3">
+                <label for="tittle" class="form-label">Tittle</label>
+                <input type="text" class="form-control" id="tittle" name="tittle">
+            </div>
+            <div class="mb-3">
+                <label for="slug" class="form-label">Slug</label>
+                <input type="text" class="form-control" id="slug" name="slug">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+@endsection
